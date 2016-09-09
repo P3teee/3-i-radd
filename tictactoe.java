7c46e661eb95e4b6c49f5cb6b21c3399287350fe
P3teee <te14.peter.berg@umea.nti.se>
2016-09-09 10:15:39 +0200

    Ett enkelt 3 i rad spel

--- tictactoe.java
+++ tictactoe.java
@@ -3,6 +3,22 @@
  */
 public class tictactoe {
     public static void main(String[] args) {
+        char[] spelplan = new char[9];
+
+        for (int i = 0 ; i < spelplan.length ; i++) {
+            spelplan[i] = " ";
+        }
+
 
     }
-}
+
+        System.out.println("---------");
+
+        for (int j = 0 ; j < 3 ; j++) {
+            System.out.print("| ");
+            for (int k = 0 ; k < 3 ; k++) {
+                System.out.print(spelplan[j][k] + " | ");
+            }
+        }
+    }
+
